a81206b5f81e8963154470f9401a4b80484114ae
koushikbardhan2000 <koushikbardhan2000@gmail.com>
2026-01-13 15:01:12 +0530

    Incorporated preflight OPTIONS request

--- API/config.php
+++ API/config.php
@@ -5,6 +5,12 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 // DB configuration
 $host     = 'localhost';
 $db_name  = 'issue';
